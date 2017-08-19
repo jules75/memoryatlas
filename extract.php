ddbd7fa3d6ad706ee37de87484c6e8c3a6de4195
Julian Laffey <julianlaffey@gmail.com>
2017-08-19 17:04:17 +1000

    Rename server side file

---
+++ extract.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Server background tasks, long-running tasks not intended to be called by web user
+ */
+
+require_once 'db.php';
+
+
+function create_coord_cache() {
+    
+    $result = [];
+    
+    foreach (get_entries()->result AS $e) {
+
+        $entry = get_entry($e->entry_id);
+        foreach($entry->ops AS $op) {
+            if (isset($op->attributes->coord)) {
+                $result[] = [
+                    'coord' => $op->attributes->coord,
+                    'title' => $op->insert,
+                    'entry_id' => $entry->entry_id
+                    ];
+            }
+        }
+
+    }
+    
+    file_put_contents('cache/coords.json', json_encode($result, JSON_PRETTY_PRINT));
+}
+
+
+function create_tag_cache() {
+    
+    $result = [];
+    
+    foreach (get_entries()->result AS $e) {
+
+        $entry = get_entry($e->entry_id);
+        foreach($entry->ops AS $op) {
+            
+            $words = preg_split("/\s/", $op->insert);
+
+            foreach($words AS $word) {
+                if ($word[0] == '#') {
+                    // same data structure as 'list' API call
+                    $result[$word]['result'][] = [
+                        'entry_id' => $entry->entry_id, 
+                        'revisions' => $e->revisions
+                        ];
+                }
+            }
+        }
+
+    }
+    
+    file_put_contents('cache/hashtags.json', json_encode($result, JSON_PRETTY_PRINT));
+}
+
+
+create_coord_cache();
+create_tag_cache();
+

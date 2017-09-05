b7d7d59c28b1b9aaf874663dafb1ad0ed09edc1b
Julian Laffey <julianlaffey@gmail.com>
2017-09-06 09:30:41 +1000

    Friendly datetime on history page

--- alpha/history.php
+++ alpha/history.php
@@ -2,18 +2,26 @@
 
 <?php require_once('../db.php'); ?>
 
+<?php
+
+function mongoIdToTimestamp($id) {
+  return hexdec(substr($id, 0, 8));
+}
+
+?>
+
 <table>
 <thead>
   <tr>
-  <td>Revision</td>
-  <td>User</td>
+  <td>Updated on</td>
+  <td>By user</td>
   <td></td>
   </tr>
   </thead>
 <?php foreach(get_entry_history($_GET['entry_id']) AS $entry): ?>
 <tr>
 <?php $url = "view.php?entry_id=" . $_GET['entry_id'] . "&revision_id=" . $entry->_id; ?>
-<td><a href="<?php echo $url ?>"><?php echo $entry->_id; ?></a></td>
+<td><a href="<?php echo $url ?>"><?php echo date('Y-m-d H:i:s', mongoIdToTimestamp($entry->_id)); ?></a></td>
 <td><?php echo $entry->user->id; ?></td>
 </tr>
 <?php endforeach; ?>

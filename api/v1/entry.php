486ac160a199bef5d3e38d2375eaabd869a03cb1
Julian Laffey <julianlaffey@gmail.com>
2017-09-19 12:36:30 +1000

    API call to delete/hide entry

--- api/v1/entry.php
+++ api/v1/entry.php
@@ -3,6 +3,9 @@
 require_once '../../config.php';
 require_once '../../db.php';
 
+// sessions required for user authentication
+session_start();
+
 
 function succeed($result)
 {
@@ -24,10 +27,44 @@ function filter_hex($string)
     return preg_replace('/[^a-fA-F0-9]+/', '', $string);
 }
 
-$entry_id = filter_hex($_GET['id']);
 
-$entry = get_entry($entry_id);
-if ($entry) {
-    succeed(['data' => $entry]);
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $entry_id = filter_hex($_GET['id']);
+
+    $entry = get_entry($entry_id);
+    if ($entry) {
+        succeed(['data' => $entry]);
+    }
+    fail("No entry found for id $entry_id");
 }
-fail("No entry found for id $entry_id");
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+    $entry_id = filter_hex($_GET['id']);
+    $entry = get_entry($entry_id);
+
+    if ($entry) {
+
+        unset($entry->_id); // strip ID to avoid duplicate error
+
+        $entry->hidden = true;
+
+        // user must be logged in to save
+        if (isset($_SESSION['user'])) {
+            $entry->user->id = $_SESSION['user']['id'];                
+            $result = insert_entry([$entry]);
+            succeed("Entry marked as hidden");
+        }
+
+        else {
+            fail("You must be logged in to delete this entry");
+        }
+
+    }
+    
+    else {
+        fail("No entry found for id $entry_id");
+    }
+
+}

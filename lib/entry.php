45c153a7e91926c98e08634a98954ffc01fd8918
Julian Laffey <julianlaffey@gmail.com>
2017-10-04 11:09:40 +1100

    No broken image if entry lacks image

--- lib/entry.php
+++ lib/entry.php
@@ -49,11 +49,8 @@ function entry_preview_html($entry_id)
     $img = cloudinaryThumbnailUrl(firstImage($entry));
     $title = title($entry);
 
-    return "<li data-entry-id='$entry_id'>
-            <a href='/alpha/entry.php?entry_id=$entry_id'>
-            <img src='$img'>
-            <span>$title</span>
-            </a>
-            </li>";
+    return "<li data-entry-id='$entry_id'><a href='/alpha/entry.php?entry_id=$entry_id'>" . 
+            ($img ? "<img src='$img'>" : "")
+            . "<span>$title</span></a></li>";
 }
 

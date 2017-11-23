bb549eb291e766b72c24c04834efee6e4d6bf3c8
Julian Laffey <julianlaffey@gmail.com>
2017-11-23 13:29:14 +1100

    Removed unused API call entryHistory

--- api/v1/entryHistory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/config.php');
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/db.php');
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/api/fns.php');
-
-$entry_id = filter_hex($_GET['id']);
-succeed(get_entry_history($entry_id));

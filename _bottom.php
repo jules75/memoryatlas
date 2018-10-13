0b528c56af24c3cb694f53fc6cbd7e561e2b96b0
Julian Laffey <julianlaffey@gmail.com>
2018-10-13 01:27:29 +0000

    Partner logos

--- _bottom.php
+++ _bottom.php
@@ -1,6 +1,19 @@
 
 </div> <!-- end content div -->
 
+<footer>
+
+<p>The Memory Atlas is made possible due to generous support from the following organisations:</p>
+<p>
+<a href="http://www.anzaccentenary.gov.au/"><img src="img/partners/anzac_centenary.png" /></a>
+<a href="https://www.dpc.vic.gov.au/"><img src="img/partners/state_gov.png" /></a>
+<a href="https://ballarat.vic.gov.au"><img src="img/partners/city_of_ballarat.png" /></a>
+<a href="https://www.seniorsonline.vic.gov.au/festivalsandawards"><img src="img/partners/vic_seniors_festival.png" /></a>
+</p>
+
+
+</footer>
+
 </body>
 
 </html>

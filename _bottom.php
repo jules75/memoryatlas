8950a163f0eca712f6a18270aff812b34acf6dfb
Julian Laffey <julianlaffey@gmail.com>
2018-10-13 21:00:56 +0000

    Fix broken link

--- _bottom.php
+++ _bottom.php
@@ -7,7 +7,7 @@
 <p>
 <a href="http://www.anzaccentenary.gov.au/"><img src="img/partners/anzac_centenary.png" /></a>
 <a href="https://www.dpc.vic.gov.au/"><img src="img/partners/state_gov.png" /></a>
-<a href="https://ballarat.vic.gov.au"><img src="img/partners/city_of_ballarat.png" /></a>
+<a href="http://ballarat.vic.gov.au"><img src="img/partners/city_of_ballarat.png" /></a>
 <a href="https://www.seniorsonline.vic.gov.au/festivalsandawards"><img src="img/partners/vic_seniors_festival.png" /></a>
 </p>
 
